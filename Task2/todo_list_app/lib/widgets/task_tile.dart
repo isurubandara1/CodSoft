6ea4b20a22382e0689ea1a9837531486943d53cc
isurubandara1 <isurumbandara@gmail.com>
2024-06-04 01:51:11 +0530

    task_title file add

---
+++ Task2/todo_list_app/lib/widgets/task_tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function(bool?) checkboxCallback;
+  final Function()? longPressCallback;
+
+  TaskTile({
+    required this.taskTitle,
+    required this.isChecked,
+    required this.checkboxCallback,
+    required this.longPressCallback,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onLongPress: longPressCallback,
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
+      ),
+    );
+  }
+}
