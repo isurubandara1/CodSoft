4d5d7ec90438ebf3bf3e4437a12571bfbaf57599
isurubandara1 <isurumbandara@gmail.com>
2024-06-13 14:52:57 +0530

    add features

--- Task3/recipe_app/lib/home_page.dart
+++ Task3/recipe_app/lib/home_page.dart
@@ -64,17 +64,63 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: DrawerPage(), // Use the DrawerPage widget here
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/Home.jpg',
-              width: double.infinity,
-              height: 800,
-              fit: BoxFit.fill,
+      body: Stack(
+        children: [
+          // Background image with opacity
+          Positioned.fill(
+            child: Container(
+              color: Colors.black.withOpacity(1),
+              child: Image.asset(
+                'assets/images/Home.jpg',
+                fit: BoxFit.cover,
+              ),
             ),
-          ],
-        ),
+          ),
+          // Content
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                    height: MediaQuery.of(context).size.height *
+                        0.2), // Adjust height to center content
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Opacity(
+                    opacity: 1,
+                    child: Text(
+                      'Unleash your inner chef with [App Name]! Explore a world of delicious recipes for every taste bud. From quick weeknight meals to adventurous global cuisine, we have something for everyone. Find dishes that fit your dietary needs, with options for vegan, vegetarian, gluten-free, and paleo eaters. Download [App Name] today - free and packed with features to turn you into a kitchen rockstar!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                    height: MediaQuery.of(context).size.height *
+                        0.2), // Adjust height to space content
+                ElevatedButton(
+                  onPressed: () {
+                    // Add your onPressed code here!
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange, // Button color
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 32, vertical: 16), // Button padding
+                    textStyle: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold), // Text style
+                  ),
+                  child: Text('Go ahead'),
+                ),
+                SizedBox(height: 16), // Space below button
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
