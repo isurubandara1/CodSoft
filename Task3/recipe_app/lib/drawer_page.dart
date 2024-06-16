587ddc30a327e3325153865ab96e88ff19bd2d42
isurubandara1 <isurumbandara@gmail.com>
2024-06-16 14:37:52 +0530

    drawer change

--- Task3/recipe_app/lib/drawer_page.dart
+++ Task3/recipe_app/lib/drawer_page.dart
@@ -1,35 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/home_page.dart';
 
 class DrawerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
+        children: [
           DrawerHeader(
+            child: Center(
+              child: Column(
+                children: [
+                  Text(
+                    'Easy Cooking',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/images/cookerman.png',
+                    fit: BoxFit.fill,
+                    height: 80,
+                    width: 120,
+                  ),
+                ],
+              ),
+            ),
             decoration: BoxDecoration(
               color: Colors.orange,
             ),
-            child: Text(
-              'Recipe App Menu',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-              ),
-            ),
           ),
           ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
+            leading: const Icon(Icons.home),
+            title: const Text('Home'),
             onTap: () {
-              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ),
+              );
             },
           ),
           ListTile(
-            leading: Icon(Icons.info),
-            title: Text('About'),
+            leading: const Icon(Icons.search),
+            title: const Text('Search'),
             onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.account_box_outlined),
+            title: const Text('About'),
+            onTap: () {
+              // Navigate to the home screen
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.share),
+            title: const Text('Share App'),
+            onTap: () {
+              // Navigate to the home screen
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.get_app),
+            title: const Text('More Apps'),
+            onTap: () {
+              // Navigate to the settings screen
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.star),
+            title: const Text('Rate App'),
+            onTap: () {
+              // Navigate to the about screen
+            },
           ),
         ],
       ),
